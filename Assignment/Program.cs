af345ba4c1dab607de69cc441d780d17ee7f318b
ayanada739 <ayanada739@gmail.com>
2024-07-25 02:35:57 +0300

    Solving Q01 [ OptimizedBubbleSort ]

---
+++ Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm  And implement the code of this optimized bubble sort algorithm
+
+            //int[] array = { 5, 3, 8, 4, 2 };
+            //OptimizedBubbleSort(array);
+
+            //Console.WriteLine("Sorted array:");
+            //foreach (var item in array)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            #endregion
+
+
+        }
+    }
+}
